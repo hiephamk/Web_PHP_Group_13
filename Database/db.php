3b9fb1accc427ff4dccd40e3500edf94686e60f0
hiephamk <142878332+hiephamk@users.noreply.github.com>
2024-02-08 18:13:33 +0200

    update name of css file

--- Database/db.php
+++ Database/db.php
@@ -1,8 +1,8 @@
 <?php
-$servername = "localhost"; // Replace with your MySQL server hostname
-$username = "hiep23000";     // Replace with your MySQL username
-$password = "eivZs7Gf";     // Replace with your MySQL password
-$dbname = "wp_hiep23000";       // Replace with the name of your MySQL database
+$servername = "web_group-24-db-1"; // Replace with your MySQL server hostname
+$username = "group_13";     // Replace with your MySQL username
+$password = "group13";     // Replace with your MySQL password
+$dbname = "group_13";       // Replace with the name of your MySQL database
 
 // Create a database connection
 $conn = new mysqli($servername, $username, $password, $dbname);
@@ -11,4 +11,3 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
-?>
--- main_page/index.php
+++ main_page/index.php
@@ -24,8 +24,7 @@ include_once "../sub_page/header.php";
             <div class="cta-col col-4">
                 <div class="cta-icon text-center">
                     <a href="https://www.youtube.com/watch?v=fdGWRq1dVBA" target="blank">
-                        <img class="img-fluid" style="width: 80px; height: 80px" src="../img/icons8-play.png"
-                            alt="Video">
+                        <img class="img-fluid" style="width: 80px; height: 80px" src="../img/icons8-play.png" alt="Video">
                         <h2>Watch videos</h2>
                     </a>
                 </div>
@@ -33,8 +32,7 @@ include_once "../sub_page/header.php";
             <div class="cta-col col-4">
                 <div class="cta-icon text-center">
                     <a href="Products.php" target="#">
-                        <img class="img-fluid" style="width: 80px; height: 80px" src="../img/icons8-find.png"
-                            alt="Product">
+                        <img class="img-fluid" style="width: 80px; height: 80px" src="../img/icons8-find.png" alt="Product">
                         <h2>Find spare parts</h2>
                     </a>
                 </div>
@@ -42,8 +40,7 @@ include_once "../sub_page/header.php";
             <div class="cta-col col-4">
                 <div class="cta-icon text-center">
                     <a href="Contact.php" target="#">
-                        <img class="img-fluid" style="width: 80px; height: 80px" src="../img/icons8-write.png"
-                            alt="Partner">
+                        <img class="img-fluid" style="width: 80px; height: 80px" src="../img/icons8-write.png" alt="Partner">
                         <h2>Become our partner</h2>
                     </a>
                 </div>
