ff48f5700a5f6421fb853983267249bc1f99d1c6
“duy23000 <duy23000@student.hamk.fi>
2024-02-09 09:46:28 +0200

    news filter

---
+++ sub_page/filter-news.php
@@ -0,0 +1,25 @@
+<?php
+include_once '../Database/db.php';
+// include '../sub_page/header.php';
+include '../sub_page/filter-data.php';
+include '../sub_page/get-category.php';
+?>
+<div class="body-page container d-inline-block justify-content-center">
+    <form method="post">
+    <select name="filterByCategory">
+        <?php
+          $getCategories = getCategory();
+          foreach($getCategories as $category) {
+        ?>
+        <option value="<?php echo $category['category_name']; ?>" 
+        <?php echo $_POST['filterByCategory']==$category['category_name']?'selected':''; ?>>
+        <?php echo $category['category_name']; ?>
+        </option>
+      <?php } ?>
+    </select>
+    <input type="submit" name="filter">
+    </form>
+</div>
+<?php
+    include '../sub_page/display-data.php';
+?>
