ec30a22da793434805a2afa87006fb7ea5ffe21a
andylovecloud <andy.tranhuynh@gmail.com>
2024-02-13 10:43:53 +0200

    update breakcrum

--- Products.php
+++ Products.php
@@ -54,6 +54,12 @@ include "header.php";
   <div class="product-container container">
     <!-- Section: Electric Car -->
     <section class="container">
+      <nav aria-label="breadcrumb">
+          <ol class="breadcrumb">
+              <li class="breadcrumb-item"><a href="index.php">Home</a></li>
+              <li class="breadcrumb-item active" aria-current="page">Products</a></li>
+          </ol>
+        </nav>
       <div class="row gx-lg-5 product-items container">
         <!-- Card Title -->
         <div class="row text-dark" id="Electric">
