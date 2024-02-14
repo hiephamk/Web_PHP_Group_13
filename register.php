fb96d8a9ebc1ad871d75625f7f2d87c50a86da16
Dinh Khanh Chi Nguyen <khanhchi.fe@gmail.com>
2024-02-14 23:02:40 +0200

    Change table field name to match with database

--- register.php
+++ register.php
@@ -8,12 +8,12 @@ include_once "header.php";
     <p><span class="error">* Required field</span></p>
     <form class="row g-3 bg-warning" action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
         <div class="col-md-6">
-            <label for="inputEmail4" class="form-label">First Name</label>
+            <label for="fname" class="form-label">First Name</label>
             <input type="text" class="form-control" id="fname" name="fname">
             <span id="fnameError" style="color: red;"></span>
         </div>
         <div class="col-md-6">
-            <label for="inputPassword4" class="form-label">Last Name</label>
+            <label for="lname" class="form-label">Last Name</label>
             <input type="text" class="form-control" id="lname" name="lname">
             <span id="lnameError" style="color: red;"></span>
         </div>
@@ -30,8 +30,8 @@ include_once "header.php";
 
         </div>
         <div class="col-6">
-            <label for="inputAddress" class="form-label">Phone Number</label>
-            <input type="text" class="form-control" name="phone" id="inputAddress" placeholder="phone number">
+            <label for="inputPhone" class="form-label">Phone Number</label>
+            <input type="text" class="form-control" name="phone" id="inputPhone" placeholder="phone number">
         </div>
         <div class="col-6">
             <label for="inputAddress" class="form-label">Address</label>
@@ -39,74 +39,72 @@ include_once "header.php";
         </div>
         <div class="col-12">
             <div class="form-check">
-                <input class="form-check-input" type="checkbox" id="gridCheck" name="term_agree">
-                <label class="form-check-label" for="gridCheck">
+                <input class="form-check-input" type="checkbox" id="gridCheck1" name="term_agree">
+                <label class="form-check-label" for="gridCheck1">
                     By checking this box, you are agreeing to our terms of service.
                 </label>
             </div>
             <div class="form-check">
-                <input class="form-check-input" type="checkbox" id="gridCheck">
-                <label class="form-check-label" for="gridCheck">
+                <input class="form-check-input" type="checkbox" id="gridCheck2">
+                <label class="form-check-label" for="gridCheck2">
                     Email me with news and updates (Option)
                 </label>
             </div>
         </div>
         <div class="col-12" align-middle>
-            <input type="submit" class="btn-primary btn fs-4"
-                style="margin:10px 0 30px 35%; width:200px; height:50px;border-radius:5px;" name="submit"
-                value="Register">
+            <input type="submit" class="btn-primary btn fs-4" style="margin:10px 0 30px 35%; width:200px; height:50px;border-radius:5px;" name="submit" value="Register">
         </div>
     </form>
 </div>
 
 <!-- JavaScript Validation  -->
 <script>
-// function to validate first name
-function validatefName() {
-    const fname = document.getElementById("fname").value;
-    const fnameError = document.getElementById("fnameError");
+    // function to validate first name
+    function validatefName() {
+        const fname = document.getElementById("fname").value;
+        const fnameError = document.getElementById("fnameError");
 
-    if (fname.length < 3 || fname.length > 20) {
-        fnameError.innerHTML = "First name must be between 3 and 20 characters!";
-        return false;
-    } else {
-        fnameError.innerHTML = "";
-        return true;
+        if (fname.length < 3 || fname.length > 20) {
+            fnameError.innerHTML = "First name must be between 3 and 20 characters!";
+            return false;
+        } else {
+            fnameError.innerHTML = "";
+            return true;
+        }
     }
-}
 
-// function to validate last name
-function validatelName() {
-    const lname = document.getElementById("lname").value;
-    const lnameError = document.getElementById("lnameError");
+    // function to validate last name
+    function validatelName() {
+        const lname = document.getElementById("lname").value;
+        const lnameError = document.getElementById("lnameError");
 
-    if (lname.length < 3 || lname.length > 20) {
-        lnameError.innerHTML = "Last name must be between 3 and 20 characters!";
-        return false;
-    } else {
-        lnameError.innerHTML = "";
-        return true;
+        if (lname.length < 3 || lname.length > 20) {
+            lnameError.innerHTML = "Last name must be between 3 and 20 characters!";
+            return false;
+        } else {
+            lnameError.innerHTML = "";
+            return true;
+        }
     }
-}
-// event listeners for real time validation
-document.getElementById("fname").addEventListener("input", validatefName);
-document.getElementById("lname").addEventListener("input", validatelName);
+    // event listeners for real time validation
+    document.getElementById("fname").addEventListener("input", validatefName);
+    document.getElementById("lname").addEventListener("input", validatelName);
 </script>
 
-<div class="container text-center">`
+<div class="container text-center"><br>
     <?php
     // Check if the 'submit' button in the form was clicked
     if (isset($_POST['submit'])) {
         // Retrieve data from the form and store it in variables
         $fname = $_POST['fname'];     // First name
-        $lname = $_POST['lname'];     // Last name
-        $addr = $_POST['address'];       // City
-        $phone = $_POST['phone'];
+        $lname = $_POST['lname'];  
         $email = $_POST['email'];
-        $pass = $_POST['password'];
+        $pass = $_POST['pass'];     
+        $phone = $_POST['phone'];
+        $addr = $_POST['addr'];  
 
         // Include the database connection file
-        include 'Database/db.php';
+        include './Database/db.php';
         if (isset($_POST['term_agree'])) {
             $check = "select * from customers where email = '$email'";
             if ($result = mysqli_query($conn, $check)) {
@@ -114,10 +112,11 @@ document.getElementById("lname").addEventListener("input", validatelName);
                 if ($rowcount >= 1) {
                     echo "<p class='text-danger'>The email has already existed</p>";
                 } else {
-                    $sql = "insert into customers(fname,lname,address,phone,email,password) values('$fname','$lname','$address','$phone','$email','$pass')";
+                    $sql = "INSERT INTO customers(fname,lname,email,pass,phone,addr) VALUES ('$fname','$lname','$email','$pass','$phone','$addr')";
                     if ($conn->query($sql) === TRUE) {
-                        echo "<p>You have successfully registered. Now you can <a href='login.php'>log in</a> or back to <a href='../main_page/index.php'>Home</a> </p>";
-                        $conn->close();
+                        echo "<p>You have successfully registered. Now you can <a href='login.php'>log in</a> or back to <a href='index.php'>Home</a> </p>";
+                    } else {
+                        echo "<p>Error: " . $sql . "<br>" . $conn->error . "</p>";
                     }
                 }
             } else {
@@ -128,7 +127,8 @@ document.getElementById("lname").addEventListener("input", validatelName);
             echo "<p class='text-danger' >Warning: You need to agree with our terms of service!</p>";
         }
     }
-
+    $conn->close();
     ?>
 </div>
+
 <?php include "footer.php"; ?>
