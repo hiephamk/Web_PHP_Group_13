ef3c851c94038a74aaca51683ecd0950844221d8
hiephamk <142878332+hiephamk@users.noreply.github.com>
2024-02-15 11:30:46 +0200

    Update db.php

--- Database/db.php
+++ Database/db.php
@@ -1,8 +1,8 @@
 <?php
 $servername = "localhost"; // Replace with your MySQL server hostname
-$username = "duy23000";     // Replace with your MySQL username
-$password = "6ynSiJMs";     // Replace with your MySQL password
-$dbname = "wp_duy23000";       // Replace with the name of your MySQL database
+$username = "";     // Replace with your MySQL username
+$password = "";     // Replace with your MySQL password
+$dbname = "";       // Replace with the name of your MySQL database
 
 // Create a database connection
 $conn = new mysqli($servername, $username, $password, $dbname);
